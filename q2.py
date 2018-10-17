7808ce57b5acead9bc99c027cbbfd32e5dfd0513
Natalie Wong <ylw16@ic.ac.uk>
2018-10-17 23:27:31 +0100

    question 2 completed

--- q2.py
+++ q2.py
@@ -1,10 +1,36 @@
-# ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
-
 import numpy as np
+import itertools
 
-#compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
-  # modify and then return the variable below
-  answer = -1
-  return answer
+    in_sumsets = sum_subsets(powerset(cashFlowIn),0)
+    out_sumsets = sum_subsets(powerset(cashFlowOut),1)
+    
+    mixed_sumsets = np.array(sorted(in_sumsets+out_sumsets))
+    num_sumsets = [num_sumsets for [num_sumsets] in mixed_sumsets[:,[0]]]
+    type_sumsets = [type_sumsets for [type_sumsets] in mixed_sumsets[:,[1]]]
+    
+    diff_num = np.diff(num_sumsets) 
+    diff_type = np.diff(type_sumsets)
+    diff_num[0] = max(diff_num)
+    for n in range(len(diff_type)):
+        if diff_type[n] == 0:
+            diff_num[n] = max(diff_num)
+    answer = min(diff_num)
+    return answer
+
+
+def powerset(iterable):
+    """computes all subsets of iterable"""
+    subsets = []
+    for r in range(len(iterable)+1):
+        subsets = subsets + list(itertools.combinations(iterable, r))
+    return subsets
+
+def sum_subsets(subsets,type):
+    sumsets = []
+    for r in range(len(subsets)):
+        sumsets.append([sum(subsets[r]),type])
+    return sumsets
+
+
 
